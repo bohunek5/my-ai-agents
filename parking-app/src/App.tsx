78cdb1e7d02a901df48eb4ce4fde462ce2ef6570
Karol Bohdanowicz <karolbohdanowicz@MacBook-Pro-Karol.local>
2026-02-02 09:59:46 +0100

    Switch to HashRouter in App.tsx

--- parking-app/src/App.tsx
+++ parking-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { Pricing } from './pages/Pricing';
@@ -9,7 +9,7 @@ import { Offer } from './pages/Offer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="panel" element={<ClientPanel />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
